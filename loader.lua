6a1ea7ca6146fae26c8863173438fdcf03ac3a7e
whyskyboxchange <93831973+whyskyboxchange@users.noreply.github.com>
2026-02-11 17:40:51 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -7,11 +7,20 @@ pcall(function()
         return game:HttpGet("https://raw.githubusercontent.com/whyskyboxchange/Hydrojeet/refs/heads/main/main.lua", true)
     end)
 
+    local success2, source2 = pcall(function()
+        return game:HttpGet("https://raw.githubusercontent.com/whyskyboxchange/Hydrojeet/refs/heads/main/watched.rbxm", true)
+    end)
+
     if success and type(source) == "string" then
+        -- Sigh...
+        pcall(makefolder, "HYROXIDE"); task.wait(); pcall(makefolder, "HYDROXIDE/bin"); task.wait(); pcall(writefile, "HYDROXIDE/bin/watched.rbxm", source2)
+        
         pcall(writefile, "hydrojeet.lua", source)
     else
         source = readfile("hydrojeet.lua")
     end
 
+    task.wait()
+    
     loadstring(source)()
 end)
